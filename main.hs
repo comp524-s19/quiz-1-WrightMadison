281e977cc38cf9ec6ed16b1c6cf48afa13d8ffc3
Madison Wright <wrightmadison@gmail.com>
2019-01-31 17:53:46 +0000

    Quiz 1 submit

---
+++ main.hs
@@ -0,0 +1,5 @@
+finalGrade :: [a] -> [a] -> Int
+finalGrade grade = let x = length(grade)) [take x grade]
+finalGrade weight = let y = length(weight)) [take y weight]
+finalGrade final = (grade/weight)/weight
+    
